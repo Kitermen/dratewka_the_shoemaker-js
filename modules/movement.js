a9fa1f4fbf6dc3d4203a825cca9c80a20ca1dc2c
Kitermen <105893213+Kitermen@users.noreply.github.com>
2024-02-21 03:43:56 +0100

    more and more improvements (walking)

--- modules/movement.js
+++ modules/movement.js
@@ -2,13 +2,21 @@ export class Location{
     constructor(data){
         this.headline = document.querySelector(".headline");
         this.location = document.querySelector(".location");
+
         this.compass = document.querySelector(".compass");
+        // this.north = document.querySelector(".NORTH");
+        // this.east = document.querySelector(".EAST");
+        // this.south = document.querySelector(".SOUTH");
+        // this.west = document.querySelector(".WEST");
+        this.directions = ["north", "east", "south", "west"];
+        
         this.direction = document.querySelector(".direction");
         this.sight = document.querySelector(".sight");
         this.carriedItem = document.querySelector(".carried-item");
 
         this.inputSpan = document.querySelector(".command-line-span");
         this.input = document.querySelector(".command-line-input");
+        this.wrapper = document.querySelector(".terminal-wrapper");
 
         this.data = data;
         this.movement = this.data[0]
@@ -16,9 +24,9 @@ export class Location{
         this.locations = this.data[2]
         this.currPosition = {x: 7, y: 4};
         this.input.addEventListener("keydown", (e)=>this.move(this.currPosition, e));
-        
-
     }
+
+    
 
     move(currPosition, e){
         if(e.key != "Enter"){
@@ -31,6 +39,18 @@ export class Location{
                 if(this.movement[currPosition.y - 1][currPosition.x - 1].moves.includes('NORTH')){
                     console.log("aaa");
                     currPosition.y -= 1;
+                    this.headline.innerHTML = this.movement[currPosition.y - 1][currPosition.x - 1].name;
+                    this.location.src = `/game_elements/img/${this.movement[currPosition.y - 1][currPosition.x - 1].image}`;
+                    this.directions.forEach(dir => {
+                        console.log(this.movement[currPosition.y - 1][currPosition.x - 1].moves);
+                        if(this.movement[currPosition.y - 1][currPosition.x - 1].moves.includes(`${dir.toUpperCase()}`)){
+                            document.getElementById(`${dir}`).style.display = "none";
+                            console.log("NOWAY", dir);
+                        }
+                        else{
+                            document.getElementById(`${dir}`).style.display = "block";
+                        }
+                    })                    
                 }
                 else{
                     
@@ -69,15 +89,18 @@ export class Location{
                 }
             break;
         }
-
-
-        console.log(currPosition);
-        this.inputSpan.innerHTML = "";
+        this.inputSpan.innerText = "";
         this.input.value = "";
-        console.log(this.movement[currPosition.y - 1][currPosition.x - 1])
-
 
     }
+
+    setUp(){
+        //this.headline.innerHTML = `${this.movement[this.currPosition.y - 1][this.currPosition.x - 1].name}`;
+        console.log("xd");
+    }
+    
+   
+    
 
 }
 
